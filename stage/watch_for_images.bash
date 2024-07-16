c2749caba0b5cf6a55e4a40777fb9cdfdeb0da93
Maria Yuryeva <137786362+mariayuyu@users.noreply.github.com>
2024-07-16 16:04:28 +0200

    Update watch_for_images.bash

--- stage/watch_for_images.bash
+++ stage/watch_for_images.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DISPLAY=:0
 WATCH_DIR="/home/pi/Pictures"
+APP_IDENTIFIER="_APPID"
 
 #fonction pour vérifier si le fichier est complètement téléchargé
 is_file_stable(){
@@ -17,3 +18,23 @@ is_file_stable(){
                 sleep 1
         done
 }
+
+inotifywait -m -e create -e moved_to --format '%w%f' "${WATCH_DIR}" | while read NEWFILE
+do
+        echo "Detected change in file: ${NEWFILE}" >> /home/pi/inotify_log.txt
+        if [[ "${NEWFILE}" =~ \.(jpg|jpeg|png|bmp|gif)$ ]]; then
+                echo "Processing image file: ${NEWFILE}" >> /home/pi/inotify_log.txt
+
+                is_file_stable "${NEWFILE}"
+                if [[ "${NEWFILE}" == *"${APP_IDENTIFIER}"* ]]; then
+                        echo "File verified: ${NEWFILE}" >> /home/pi/inotify_log.txt
+                        chmod 644 "${NEWFILE}"
+                        feh --fullscreen --hide-pointer "${NEWFILE}" &
+                        echo "feh --fullscreen --hide-pointer '${NEWFILE}' &" > /home/pi/last_feh_command.sh
+                else
+                        echo "File rejected: ${NEWFILE}" >> /home/pi/inotify_log.txt
+                        rm "${NEWFILE}"
+                fi
+        fi
+done
+
